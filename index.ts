9a866fd44cf50312c550d9bfd08e5a3c6500a927
handagenalini <handagenalini@gmail.com>
2023-04-13 19:50:39 +0530

    backend

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
+const numelement=document.getElementById('num1') as HTMLInputElement;
+const numelement2=document.getElementById('num2') as HTMLInputElement;
+const buttonelement=document.querySelector('button');
+
 function add(num1:number,num2:number){
     return num1+num2
 }
-console.log(add(1,2))
-add('1','2')
+buttonelement.addEventListener('click',()=>{
+const num1=numelement.value
+const num2=numelement2.value
+const result=add(+num1,+num2)
+console.log(result)
+
+})
+
